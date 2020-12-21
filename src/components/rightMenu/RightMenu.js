42230fcceaa0f5ff647629afa89d8004f22a5562
LaryCxb <przem.kruk@gmail.com>
2020-12-21 13:18:15 +0100

    B035 | RightMenu rebuilt with Grid

--- src/components/rightMenu/RightMenu.js
+++ src/components/rightMenu/RightMenu.js
@@ -1,25 +1,38 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-import { MenuLayout } from "../../styles/menuStyles";
+import {Grid} from "@material-ui/core";
+import Box from "@material-ui/core/Box";
 
 const RightMenu = () => (
-  <MenuLayout>
-    <Button variant="outlined" color="primary">
-      Stwórz projekt
-    </Button>
-    <Button variant="outlined" color="primary">
-      Dodaj kod
-    </Button>
-    <Button variant="outlined" color="primary">
-      Szukaj pracy
-    </Button>
-    <Button variant="outlined" color="primary">
-      Dodaj post na forum
-    </Button>
-    <Button variant="outlined" color="primary">
-      Nabór do projektu
-    </Button>
-  </MenuLayout>
+    <Box p={2}>
+        <Grid container spacing={1}>
+            <Grid item xs={12}>
+                <Button variant="outlined" color="primary" fullWidth>
+                    Stwórz projekt
+                </Button>
+            </Grid>
+            <Grid item xs={12}>
+                <Button variant="outlined" color="primary" fullWidth>
+                    Dodaj kod
+                </Button>
+            </Grid>
+            <Grid item xs={12}>
+                <Button variant="outlined" color="primary" fullWidth>
+                    Szukaj pracy
+                </Button>
+            </Grid>
+            <Grid item xs={12}>
+                <Button variant="outlined" color="primary" fullWidth>
+                    Dodaj post na forum
+                </Button>
+            </Grid>
+            <Grid item xs={12}>
+                <Button variant="outlined" color="primary" fullWidth>
+                    Nabór do projektu
+                </Button>
+            </Grid>
+        </Grid>
+    </Box>
 );
 
 export default RightMenu;
